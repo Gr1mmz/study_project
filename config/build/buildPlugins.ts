b2fe28940244da3f73634dfdf2bb862c4c2e7742
Gr1mmz <admin@kraevskiy-web.ru>
2023-02-07 12:49:38 +0800

    add hot reload and change font and colors

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,6 +1,7 @@
 import webpack from 'webpack';
 import HTMLWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import { BuildOptions } from './types/config';
 
 export function buildPlugins(options: BuildOptions): webpack.WebpackPluginInstance[] {
@@ -16,6 +17,9 @@ export function buildPlugins(options: BuildOptions): webpack.WebpackPluginInstan
     }),
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
-    })
+    }),
+    isDev 
+      ? new ReactRefreshWebpackPlugin() 
+      : new webpack.HotModuleReplacementPlugin(),
   ];
 };
