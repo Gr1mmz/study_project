871b9c745324f8949df31878ecf0cc96225b1fda
Gr1mmz <admin@kraevskiy-web.ru>
2023-02-28 11:11:53 +0800

    fix dev plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,4 +1,3 @@
-import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import HTMLWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import webpack from 'webpack';
@@ -10,7 +9,8 @@ export function buildPlugins(
   options: BuildOptions,
 ): webpack.WebpackPluginInstance[] {
   const { paths, isDev } = options;
-  return [
+
+  const plugins = [
     new HTMLWebpackPlugin({
       template: paths.html,
     }),
@@ -22,11 +22,14 @@ export function buildPlugins(
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
     }),
-    isDev
-      ? new ReactRefreshWebpackPlugin({ overlay: false })
-      : new webpack.HotModuleReplacementPlugin(),
-    new BundleAnalyzerPlugin({
-      openAnalyzer: false,
-    }),
   ];
+
+  if (isDev) {
+    plugins.push(new webpack.HotModuleReplacementPlugin());
+    plugins.push(new BundleAnalyzerPlugin({
+      openAnalyzer: false,
+    }));
+  }
+
+  return plugins;
 }
