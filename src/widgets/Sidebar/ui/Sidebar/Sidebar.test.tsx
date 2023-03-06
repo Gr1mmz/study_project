3f4c22b5c4cb9d88ea0f062891629bfc11472078
Gr1mmz <admin@kraevskiy-web.ru>
2023-03-06 12:36:36 +0800

    fix tests

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,17 +1,17 @@
 import { fireEvent, screen } from '@testing-library/react';
 import {
-  renderWithTranslation,
-} from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+  componentRender,
+} from 'shared/config/jest/componentRender/componentRender';
 import { Sidebar } from 'widgets/Sidebar';
 
 describe('Sidebar', () => {
   test('render', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
   });
 
   test('render', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     const toggleBtn = screen.getByTestId('sidebar-toggle');
     expect(screen.getByTestId('sidebar-toggle')).toBeInTheDocument();
     fireEvent.click(toggleBtn);
